2cee76375c1a7005421c5bc6e309be292d4759d5
Yothin <e20utt@hotmail.com>
2020-10-07 17:34:51 +0700

    add foodstory product

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv").config();
 const FlowAccount = require("./libs/flowacc");
 const ProductMap = require("./libs/productmap");
 const FoodStory = require("./libs//foodstory/foodstory");
+const XLSX = require("xlsx");
 
 (async() => {
 
@@ -35,7 +36,21 @@ const FoodStory = require("./libs//foodstory/foodstory");
     let fd = new FoodStory();
     //await fd.connect(process.env.FOODSTORY_USERNAME, process.env.FOODSTORY_PASSWORD);
 
-    let res = await fd.getMenuPerPage(1, 24);
-    console.log(JSON.stringify(res));
+    let menu = [];
+    for (let i = 1; i<=11; i++) {
+        let res = await fd.getMenuPerPage(7171, i, 12, 1);
+        console.log(res.data.length);
+        menu = [...menu, ...res.data];
+    }
+
+    //console.log(menu);
+    console.log(menu.length);
+    let wb = XLSX.utils.book_new();
+    let ws = XLSX.utils.json_to_sheet(menu);
+    XLSX.utils.book_append_sheet(wb, ws, "menu");
+
+    await XLSX.writeFile(wb, "menu.xlsx");
+
+    // console.log(JSON.stringify(res));
 
 })();
